62fd4f64cf8eb558876e792acb5aacce17e4bf75
Camilo Balaguera <ca.balaguera@uniandes.edu.co>
2018-05-10 17:19:45 -0500

    Se corrigió la creacion del mp4

--- punto_2/CB_graph.py
+++ punto_2/CB_graph.py
@@ -11,10 +11,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+
 data = np.genfromtxt("datos.dat", delimiter = " ")
 datos = data[1:]
 nx = data[0][0] - 2 # Tamaño de la dimension espacial
-nt = data[0][1] # Tamaño de la dimension temporal.
+nt = int(data[0][1]) # Tamaño de la dimension temporal.
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
@@ -32,12 +33,11 @@ def animate(i):
     return line,
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,interval = 10, blit=True)
+anim = animation.FuncAnimation(fig, animate, init_func=init,frames = nt, blit=True)
 
 # save the animation as an mp4.  This requires ffmpeg or mencoder to be
 # installed.  The extra_args ensure that the x264 codec is used, so that
 # the video can be embedded in html5.  You may need to adjust this for
 # your system: for more information, see
 # http://matplotlib.sourceforge.net/api/animation_api.html
-anim.save('cuerda.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
-plt.show()
+anim.save('cuerda.mp4', fps=250)
